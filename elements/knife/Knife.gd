5ce161caf13e03a794dc397078e722b3308daaf1
zhenych <droid.shmtrx@gmail.com>
2023-09-06 20:50:07 +0300

    3-4

--- elements/knife/Knife.gd
+++ elements/knife/Knife.gd
@@ -3,35 +3,24 @@ extends CharacterBody2D
 var is_flying := false
 var speed := 4500.0
 
-func _physics_process(delta):
+func _physics_process( delta):
 	if is_flying:
-		var collision := move_and_collide(Vector2.UP * speed * delta)
+		var collision := move_and_collide( Vector2.UP * speed * delta)
+		if collision:
+			handle_collision( collision)
 
 func throw():
 	is_flying = true
 	
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-## Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-#
-#
-#func _physics_process(delta):
-#	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y += gravity * delta
-#
-#	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-#
-#	# Get the input direction and handle the movement/deceleration.
-#	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction = Input.get_axis("ui_left", "ui_right")
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-#	move_and_slide()
+func handle_collision( collision: KinematicCollision2D):
+	var collider := collision.get_collider()
+	if collider is Target:
+		add_knife_to_target( collider)
+		is_flying = false
+
+func add_knife_to_target( target: Target):
+	get_parent().remove_child( self)
+	global_position = target.KNIFE_POSITION
+	rotation = 0
+	target.add_object_with_pivot(self, -target.rotation)
+	
