c2937f5fc2861c0ebef6faf51ad73ea1a317f72e
zhenych <droid.shmtrx@gmail.com>
2023-09-11 15:38:38 +0300

    3-10

--- core/globals.gd
+++ core/globals.gd
@@ -4,6 +4,6 @@ var rng := RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	rng.randomize()
-	rng.seed = -2364680567785300547
+	rng.randomize()
+#	rng.seed = -2364680567785300547
 	print_debug(rng.seed)
--- elements/apple/apple.gd
+++ elements/apple/apple.gd
@@ -1,8 +1,30 @@
 extends Node2D
 
+const EXPLOSION_TIME := 1.0
+
 var is_hited := false
+
+
+@onready var sprite := $Sprite2D
+@onready var apple_particles := [
+	$AppleParticles2D,
+	$AppleParticles2D2
+]
 
 func _on_area_2d_body_entered(body):
 	if not is_hited:
 		is_hited = true
-		queue_free()
+		apple_explode()		
+
+func apple_explode():
+	sprite.hide()
+	
+	var tween := create_tween()
+	
+	for _item in apple_particles:
+		var apple : CPUParticles2D = _item
+		apple.emitting = true
+		tween.parallel().tween_property(apple, "modulate", Color("ffffff00"), EXPLOSION_TIME)
+	tween.play()
+	await tween.finished
+	queue_free()
