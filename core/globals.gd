824bb043375779e4a96cb258bb14b6ae59478536
zhenych <droid.shmtrx@gmail.com>
2023-09-15 12:24:37 +0300

    5-1
    scenes navigation

--- core/globals.gd
+++ core/globals.gd
@@ -1,9 +1,20 @@
 extends Node
 
+const location_to_scene = {
+	Events.LOCATIONS.GAME: preload("res://scenes/game/game.tscn"),
+	Events.LOCATIONS.SHOP: preload("res://scenes/knife_shop/knife_shop.tscn"),
+	Events.LOCATIONS.START: preload("res://scenes/start_screen/start_screen.tscn")
+}
+
 var rng := RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
 #	rng.seed = -2364680567785300547
-	print_debug(rng.seed)
+	print_debug( rng.seed)
+	
+	Events.location_changed.connect(handle_location_change)
+
+func handle_location_change( location: Events.LOCATIONS):
+	get_tree().change_scene_to_packed(location_to_scene.get(location))
