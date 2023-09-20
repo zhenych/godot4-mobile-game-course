e41b6d26b4d67ff80874d2a19e6fe4bc988a7f43
zhenych <droid.shmtrx@gmail.com>
2023-09-20 12:01:03 +0300

    5-8

--- core/globals.gd
+++ core/globals.gd
@@ -5,6 +5,9 @@ const location_to_scene = {
 	Events.LOCATIONS.SHOP: preload("res://scenes/knife_shop/knife_shop.tscn"),
 	Events.LOCATIONS.START: preload("res://scenes/start_screen/start_screen.tscn")
 }
+const SAVE_GAME_FILE := "user://savegame.save"
+const SAVE_VARIABLES := ["apples"]
+
 const MAX_STAGE_APPLES := 3
 const MAX_STAGE_KNIVES := 2
 const MIN_KNIVES := 5
@@ -19,11 +22,40 @@ var apples := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_game()
 	rng.randomize()
 #	rng.seed = -2364680567785300547
 	print_debug( rng.seed)
 	
 	Events.location_changed.connect( handle_location_change)
+
+func save_game():
+	var save_game_file = FileAccess.open( SAVE_GAME_FILE, FileAccess.WRITE)
+	if save_game_file == null:
+		printerr( "Save file failed with error code {0}".format( [FileAccess.get_open_error()]))
+		return
+	var game_data := {}
+	for variable in SAVE_VARIABLES:
+		game_data[variable] = get(variable)
+	var json_object = JSON.new()
+	save_game_file.store_line(json_object.stringify(game_data))
+	
+func load_game():
+	if not FileAccess.file_exists(SAVE_GAME_FILE):
+		return
+	var save_game_file = FileAccess.open( SAVE_GAME_FILE, FileAccess.READ)
+	if save_game_file == null:
+		printerr( "Read file failed with error code {0}".format( [FileAccess.get_open_error()]))
+		return
+	var json_object := JSON.new()
+	var error = json_object.parse( save_game_file.get_line())
+	if error != OK:
+		return
+	var game_data = json_object.get_data()
+	for variable in SAVE_VARIABLES:
+		if variable in game_data:
+			set( variable, game_data[variable])
+	
 
 func add_apples( amount:int):
 	apples += amount
